259941efa2420eb36a9a2b649295cba1f475ef88
Jmire927 <117117359+Jmire927@users.noreply.github.com>
2022-11-02 15:11:24 -0500

    added the Variables that i know we need so far

--- Jam_Eez_enclouser_project.ino
+++ Jam_Eez_enclouser_project.ino
@@ -1,3 +1,25 @@
+// initiating the global variables
+//LED Variables
+Const int ledcoin = 13
+Const int ledToken1 = 12
+Const int ledToken2 = 11  
+
+//Servo variables
+#include <Servo.h>
+
+//Cereberus
+Servo cerebytail;
+Const int buttonPin = 2;
+
+//Charon
+Servo charonMove;
+Const int buttonPin = 3;
+
+//Good Ending Wall
+Servo goodEnd;
+Const int buttonPin =4;
+
+
 void setup() {
   // put your setup code here, to run once:
 }
