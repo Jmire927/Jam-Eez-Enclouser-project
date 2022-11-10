650eb78d86c164e1f559c0404c04d0c534894fe2
Jmire927 <117117359+Jmire927@users.noreply.github.com>
2022-11-10 12:38:24 -0600

    fixed syntax

--- Jam_Eez_enclouser_project.ino
+++ Jam_Eez_enclouser_project.ino
@@ -1,28 +1,28 @@
 //LED Variables
-Const int ledcoin = 13
-Const int buttonCoin = 5
+const int ledcoin = 13;
+const int buttonCoin = 5;
 
-Const int ledToken1 = 12
-Const int buttonToken1 = 6
+const int ledToken1 = 12;
+const int buttonToken1 = 6;
 
-Const int ledToken2 = 11  
-Const int buttonToken2 = 7
+const int ledToken2 = 11  ;
+const int buttonToken2 = 7;
 
 
 //Servo variables
-#include <Servo.h>
+#include <Servo.h>;
 
 //Cerberus
-Servo cerebytail;
-Const int buttonCerby = 2;
+Servo cerbytail;
+const int buttonCerby = 2;
 
 //Charon
 Servo charonMove;
-Const int buttonCharon = 3;
+const int buttonCharon = 3;
 
 //Good Ending Wall
 Servo goodEnd;
-Const int buttonGoodEnd =4;
+const int buttonGoodEnd =4;
 
 //----------------------------------------------------
 void setup() {
@@ -35,28 +35,28 @@ charonMove.attach(9);
 pinMode(buttonCharon, INPUT);
 
 //good ending wall moving
-goodEnd.attach(10)
+goodEnd.attach(10);
   pinMode(buttonGoodEnd, INPUT);
 }
 //-----------------------------------------------------
 void loop() {
 
 //Cereberus tail movement
-if (digitalRead(buttonCerby) == HIGH{
+if (digitalRead(buttonCerby) == HIGH){
   cerbytail.write(135);
 } else {
   cerbytail.write(45);
 }
 
 //Charon movement
-    if (digitalRead(buttonCharon) == HIGH{
+    if (digitalRead(buttonCharon) == HIGH){
   charonMove.write(135);
 } else {
   charonMove.write(0);
 }
 
 //Good end movement
-    if (digitalRead(buttonGoodEnd) == HIGH{
+    if (digitalRead(buttonGoodEnd) == HIGH){
   goodEnd.write(135);
 } else {
   goodEnd.write(0);
